4d3ae8b99cb6194fa053c3c0ee3edc3656475574
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-09-04 04:22:26 +0100

    feat(app): setting up husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import './App.css';
-
 function App() {
    return (
       <div className="App">
          <header className="App-header">
-            <img src={""} className="App-logo" alt="logo" />
+            <img src={''} className="App-logo" alt="logo" />
             <p>
                Edit <code>src/App.tsx</code> and save to reload.
             </p>
